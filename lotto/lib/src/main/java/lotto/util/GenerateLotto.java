97806ead64b7e1f2d25fa8aefe1090f96e576ecc
KimSeokjin <7271kim@naver.com>
2021-11-22 16:13:52 +0900

    [Lotto] 특정 숫자를 제외하고 로또 뽑는 생성기 추가

--- lotto/lib/src/main/java/lotto/util/GenerateLotto.java
+++ lotto/lib/src/main/java/lotto/util/GenerateLotto.java
@@ -3,6 +3,7 @@ package lotto.util;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -11,6 +12,8 @@ public class GenerateLotto {
     private static final int START = 1;
     private static final int END = 45;
     private static final int LENGTH = 6;
+    private static final String CHECK_EXCLUDEDNUMBER = String
+        .format("최대 %d개수 만큼 제외 가능합니다", END - LENGTH);
 
     private static final List<Integer> LOTTO_RANGE = IntStream
         .rangeClosed(START, END).boxed()
@@ -19,6 +22,22 @@ public class GenerateLotto {
     public static List<Integer> create() {
         Collections.shuffle(LOTTO_RANGE);
         return new ArrayList<>(LOTTO_RANGE.subList(0, LENGTH));
+    }
+
+    public static List<Integer> create(Set<Integer> excludedNumber) {
+        if (excludedNumber == null || excludedNumber.size() > END - LENGTH) {
+            throw new IllegalArgumentException(CHECK_EXCLUDEDNUMBER);
+        }
+        List<Integer> excludedRange = limitRange(excludedNumber);
+        return new ArrayList<>(excludedRange.subList(0, LENGTH));
+    }
+
+    private static List<Integer> limitRange(Set<Integer> excludedNumber) {
+        Collections.shuffle(LOTTO_RANGE);
+        return LOTTO_RANGE.stream()
+            .filter(number -> excludedNumber.stream()
+                .noneMatch(number::equals))
+            .collect(Collectors.toList());
     }
 
 }
--- lotto/lib/src/test/java/lotto/util/GenerateLottoTest.java
+++ lotto/lib/src/test/java/lotto/util/GenerateLottoTest.java
@@ -1,10 +1,12 @@
 package lotto.util;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 
-import org.assertj.core.util.Arrays;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
@@ -36,11 +38,42 @@ public class GenerateLottoTest {
                 numbers[lotto.get(index) - 1] = 1;
             }
         }
-        boolean isFail = Arrays.asList(numbers).stream()
-            .filter(number -> (int)number == 0)
+        boolean isFail = Arrays.stream(numbers)
+            .filter(number -> number == 0)
             .findAny()
             .isPresent();
 
         assertThat(isFail).isFalse();
+    }
+
+    @Test
+    @DisplayName("특정 번호가 제외되고 뽑혓는지 확인")
+    void 특정번호가_제외되었는지_테스트() {
+        int[] numbers = new int[45];
+        Set<Integer> excludedNumber = Set.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,
+            12, 13, 14, 15, 16, 17);
+        for (int loop = 0; loop < 500; loop++) {
+            List<Integer> lotto = GenerateLotto.create(excludedNumber);
+            for (int index = 0; index < lotto.size(); index++) {
+                numbers[lotto.get(index) - 1] = 1;
+            }
+        }
+        boolean isFail = excludedNumber.stream()
+            .filter(number -> numbers[number - 1] == 1)
+            .findAny()
+            .isPresent();
+
+        assertThat(isFail).isFalse();
+    }
+
+    @Test
+    @DisplayName("40개 이상의 번호를 제외하고 로또구매는 불가능하다.")
+    void 특정번호가_제외_유효성_테스트() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Set<Integer> excludedNumber = Set.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+                11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26,
+                27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40);
+            GenerateLotto.create(excludedNumber);
+        });
     }
 }
