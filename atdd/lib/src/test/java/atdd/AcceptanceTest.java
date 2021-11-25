1c97ccd99b30bcb252cf84b6fd185662a141fb96
KimSeokjin <7271kim@naver.com>
2021-11-25 11:31:12 +0900

    [ATDD] 인수테스트를 위한 DB클랜징 유틸 추가

---
+++ atdd/lib/src/test/java/atdd/AcceptanceTest.java
@@ -0,0 +1,28 @@
+package atdd;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+
+import atdd.util.DatabaseCleanUp;
+import io.restassured.RestAssured;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class AcceptanceTest {
+
+    @LocalServerPort
+    int port;
+
+    @Autowired
+    DatabaseCleanUp databaseCleanUp;
+
+    @BeforeEach
+    public void setUp() {
+        if (RestAssured.port == RestAssured.UNDEFINED_PORT) {
+            RestAssured.port = port;
+            databaseCleanUp.getTablesName();
+        }
+        databaseCleanUp.cleanDB();
+    }
+}
